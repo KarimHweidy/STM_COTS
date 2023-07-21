830b6d5b4fb9387454fa4bf106961dfe1f2a9150
KarimHweidy97 <134971286+KarimHweidy97@users.noreply.github.com>
2023-07-21 17:57:42 +0300

    including interface and private header file

---
+++ STM32F446RE/GPIO driver/Inc/GPIO_priv.h
@@ -0,0 +1,26 @@
+#ifndef GPIO_PRIV_H
+#define GPIO_PRIV_H
+
+
+
+
+#define GPIO_PERIPHERAL_NUM     8u
+
+#define MODER_MASK              0b11
+
+#define MODER_PIN_ACCESS        2u
+
+#define PUPDR_MASK              0b11
+#define PUPDR_PIN_ACCESS        2u
+
+#define OTYPER_MASK                1u
+
+#define OSPPEEDR_MASK              0b11
+#define OSPPEEDR_PIN_ACCESS        2u
+
+#define AFR_MASK                   0b1111
+#define AFR_PIN_ACCESS             4u
+#define AFR_ACCESS                 8u
+
+
+#endif
